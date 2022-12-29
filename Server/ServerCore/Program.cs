59e1cc2cbaab596f68af3d95db42110948e95fa6
sarah2234 <2234sarah@gmail.com>
2022-12-30 01:17:37 +0900

    섹션 1. 멀티쓰레드 프로그래밍-컴파일러 최적화

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -5,56 +5,40 @@ namespace ServerCore
 {
     class Program
     {
-        static void MainThread(object state)
+        // volatile: 언제 바뀔지 모르는 값 => 최적화하지 않음
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("스레드 시작!");
+
+            // Release Mode에서 while (_stop == false) 코드 (최적화)
+            // if (_stop == false)
+            //     while (true)
+            // 따라서 아래의 코드는 최적화 대상이 되면 안됨(_stop의 값이 도중에 바뀔 수 있으므로)
+
+            while(_stop == false)
             {
-                Console.WriteLine("Hello Thread!");
+                // 누군가가 stop 신호를 해주기를 기다린다.
             }
+            Console.WriteLine("스레드 종료!");
         }
 
         static void Main(string[] args)
         {
-            // SetMinThreads: ThreadPool이 만들 수 있는 스레드의 최소 개수 설정
-            // Max 개수를 넘어갈 시 나머지 스레드는 대기 상태
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            for (int i = 0; i < 5; i++) 
-            {
-                // TaskCreationOptions.LongRunning: 오래 걸리는 작업이라는 것을 알림
-                Task task = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
-                task.Start(); // ThreadPool에 넣어서 스레드 관리
-            }
+            Thread.Sleep(1000);
 
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { Console.WriteLine("."); } });
-            //}
+            _stop = true;
 
-            // QueueUserWorkItem: 실행을 위해 메서드를 큐에 대기시킴
-            // 가급적 짧게 일하는 스레드를 ThreadPool에 사용하는 것이 cpu 독점 문제 예방 => 이를 해결하기 위해서 Task에서 LongRunning 사용
-            ThreadPool.QueueUserWorkItem(MainThread);
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기 중");
 
-            //for (int i = 0; i < 1000; i++)
-            //{
-            //    Thread t = new Thread(MainThread);
-            //    t.Name = "Test Thread";
-            // C#에서 스레드는 기본적으로 foreground(isBackground == false)
-            //    t.IsBackground = true;
-            //    t.Start();
-            //}
+            t.Wait();
 
-            //Console.WriteLine("Waiting for Thread!"); 
-
-            // Join(): 스레드가 끝날 때까지 기다렸다가 아래줄 실행
-            //t.Join();
-            //Console.WriteLine("Hello World!");
-
-            while (true)
-            {
-
-            }
+            Console.WriteLine("종료 성공");
         }
     }
 }
