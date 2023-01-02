457372a6dbc4077181ea0f29c4ffe2a593a40e5b
sarah2234 <2234sarah@gmail.com>
2023-01-02 11:39:46 +0900

    섹션 1. 멀티쓰레드 프로그래밍 - Lock 기초

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -6,42 +6,49 @@ namespace ServerCore
     class Program
     {
         static int number = 0;
+        static object _obj = new object();
 
         static void Thread_1()
         {
-            // atomic(원자성) : 동작은 모두 성공하거나 실패해야 함
-
-            // 어셈블리어에서 number++;는
-            // int temp = number;
-            // temp += 1;
-            // number = temp;로 쪼개짐
 
             for (int i = 0; i < 10000; i++)
             {
-                // Interlocked 내부에 간접적으로 메모리 배리어를 사용하므로 가시성 문제 해결 (volatile 필요 x) 
-                int value = Interlocked.Increment(ref number); // 성능 손해 but atomic
+                // 상호 배제(Mutual Exclusive): 임계 구역을 단 한 개의 프로세스만이 접근할 수 있도록 함
+                // 코드의 가독성을 위해 try-finally 사용
+                // 하지만 좀 더 편하게 lock 키워드 사용!!
+                //try
+                //{
+                //    Monitor.Enter(_obj); // 문을 잠그는 행위
+                //    number++;
 
-                //int temp = number; // 0
-                //temp += 1; // 1
-                //number = temp; // Thread_1의 코드가 먼저 실행됐을 때: number = 1;
-                //// 위의 세 코드가 한번에 실행되지 않아 number의 값이 정확해지지 않는 문제 발생
+                //    return;
+                //}
+                //finally
+                //{
+                //    Monitor.Exit(_obj); // 잠금을 푸는 행위
+                //}
+
+                lock (_obj)
+                {
+                    number++;
+                }
             }
         }
 
+        // Deadlock: 둘 이상의 프로세스가 다른 프로세스가 점유하고 있는 자원을 서로 기다릴 때 무한 대기에 빠지는 상황
+        // deadlock을 피하기 위해 Monitor.Enter 후 반드시 Monitor.Exit 추가
         static void Thread_2()
         {
-            // 어셈블리어에서 number--;는
-            // int temp = number;
-            // temp -= 1;
-            // number = temp;로 쪼개짐
-
             for (int i = 0; i < 10000; i++)
             {
-                Interlocked.Decrement(ref number);
+                //Monitor.Enter(_obj);
+                //number--;
+                //Monitor.Exit(_obj);
 
-                //int temp = number; // 0
-                //temp -= 1; // -1
-                //number = temp; // Thread_2의 코드가 먼저 실행됐을 때: number = -1;
+                lock(_obj)
+                {
+                    number--;
+                }
             }
         }
 
