fb84306f740305788074f52bd0e7bbc874df0ebf
sarah2234 <2234sarah@gmail.com>
2023-01-03 11:10:43 +0900

    섹션 1. 멀티쓰레드 프로그래밍 - SpinLock

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -3,70 +3,62 @@ using System.Threading;
 
 namespace ServerCore
 {
-    // DeadLock을 완전히 막을 수는 없다.
-    // DeadLock이 발생한 것을 확인 후 코드 수정이 효율적
-
-    // DeadLock 예제
-    class SessionManager
+    class SpinLock
     {
-        static object _lock = new object();
-
-        static public void TestSession()
+        volatile int _locked = 0;
+        public void Acquire()
         {
-            lock (_lock)
+            while (true)
             {
+                // 1.
+                // original 변수 위치: stack / _locked 변수 위치: data
+                // Interlocked.Exchange: location1을 value 값으로 설정
+                // 리턴: location1의 원래 값
+                //int original = Interlocked.Exchange(ref _locked, 1);
+                //if (original == 0)
+                //    break;
 
+                // 2.
+                // 위의 코드처럼 조건 없이 변수 값을 바꾸는 것은 위험할 수 있음
+                // Interlocked.CompareExchange: location1과 comparand의 값이 같으면 location1의 값을 value 값으로 바꿈
+                // 리턴: location1의 원래 값
+                // if (_locked == 1) _locked = 0;과 동일
+                int expected = 0;
+                int desired = 1;
+                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected)
+                    break;
             }
         }
 
-        static public void Test()
+        public void Release() 
         {
-            lock (_lock)
-            {
-                UserManager.TestUser();
-            }
-        }
-    }
-
-    class UserManager
-    {
-        static object _lock = new object();
-
-        static public void Test()
-        {
-            lock (_lock)
-            {
-                SessionManager.TestSession();
-            }
-        }
-
-        static public void TestUser()
-        {
-            lock (_lock)
-            {
-
-            }
+            // 잠금 해제
+            _locked = 0;
         }
     }
 
     class Program
     {
-        static int number = 0;
-        static object _obj = new object();
+        static int num = 0;
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                SessionManager.Test();
+                _lock.Acquire();
+                num++;
+                _lock.Release();
             }
         }
 
-        static void Thread_2()
+        static void Thread_2() 
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                UserManager.Test();
+                _lock.Acquire();
+                num--;
+                _lock.Release();
             }
         }
 
@@ -75,12 +67,11 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
-            //Thread.Sleep(1000); // 실행 시작 시간이 다르면 deadlock이 발생하지 않음
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(number);
+            Console.WriteLine(num);
         }
     }
 }
