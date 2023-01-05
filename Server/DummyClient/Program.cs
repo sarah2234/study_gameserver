a4cc1994d91bfde402073687a9a4513c89a335a5
sarah2234 <2234sarah@gmail.com>
2023-01-06 00:37:41 +0900

    섹션 2. 네트워크 프로그래밍 - 소켓 프로그래밍 입문 #2

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
@@ -6,7 +9,34 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected to {socket.RemoteEndPoint.ToString()}");
+
+                byte[] sendBuffer = Encoding.UTF8.GetBytes("Hello World!");
+                int sendBytes = socket.Send(sendBuffer);
+
+                byte[] recvBuffer = new byte[1024];
+                int recvBytes = socket.Receive(recvBuffer);
+                string recvData = Encoding.UTF8.GetString(recvBuffer, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
         }
     }
 }
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,37 +1,67 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
 namespace ServerCore
 {
-    /*
-     * Thread Local Storage
-     * TLS란 스레드가 각각 가지고 있는 저장소이다.
-     * 이 곳에 저장되는 값들은 전역 변수이지만, 한 스레드 내에서만 유효한 전역 변수이다.
-     */
     class Program
     {
-        // ThreadLocal<> 인자로 func delegate를 받으면 TLS를 새로 만들 때 ThreadLocal.Value에 return 값을 넣어준다.
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My name is {Thread.CurrentThread.ManagedThreadId}"; });
-
-        static void WhoAmI()
-        {
-            // 이미 만들어진 ThreadName을 재사용함
-            bool repeat = ThreadName.IsValueCreated;
-            if (repeat)
-                Console.WriteLine(ThreadName.Value + "(repeat)");
-            else // ThreadName.Value == null일 때 인자로 받은 func delegate 실행
-                Console.WriteLine(ThreadName.Value); 
-        }
-        
         static void Main(string[] args) 
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            // Parallel.Invoke(): 여러 작업들을 병렬로 처리하는 기능을 제공한다.
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            // IPHostEntry: 호스트 이름, 등록되어 있는 ip 주소들 등 호스트에 대한 DNS 정보 포함
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            // ip 주소는 여러 개 존재 가능 (트래픽이 많을 시 각 이용자에게 다른 ip 주소 할당)
+            IPAddress ipAddr = ipHost.AddressList[0];
+            // endPoint: 최종 주소
+            // address(ipAddr): 인터넷 호스트의 IP 주소
+            // port(7777): address와 연결된 포트 번호이거나, 사용할 수 있는 포트가 있을 시 0으로 지정 
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            // ThreadLocal 인스턴스가 사용하는 리소스들을 해제함
-            ThreadName.Dispose();
+            // TCP 소켓 객체 생성
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // 소켓에 필요한 정보(ip 주소, 포트 번호) 바인딩
+                listenSocket.Bind(endPoint);
+
+                // Listen: 소켓을 listen(클라이언트의 연결 요청을 기다리는 상태)로 만든다.
+                // backlog: 최대 대기 수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // 클라이언트의 연결 요청을 받음
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // 메세지를 받음
+                    byte[] recvBuffer = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuffer);
+                    // Encoding.GetString: 지정한 바이트 배열의 모든 바이트를 문자열로 디코딩
+                    // index에서 시작하여 count 길이만큼의 바이트 시퀀스를 문자열로 디코딩
+                    string recvData = Encoding.UTF8.GetString(recvBuffer, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    // 메세지를 보냄
+                    byte[] sendBuffer = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                    clientSocket.Send(sendBuffer);
+
+                    // 통신 종료
+                    // SocketShutdown.Both: 소켓의 송수신을 모두 사용하지 않음
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
